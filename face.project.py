7dd4e1e492001883ebbc7621bb2845a203dab557
rishi0576 <101387909+rishi0576@users.noreply.github.com>
2022-03-18 11:51:18 +0530

    Add files via upload

---
+++ face.project.py
@@ -0,0 +1,31 @@
+import pygame, sys
+
+pygame.init()
+clock=pygame.time.Clock()
+
+screen = pygame.display.set_mode((400,600))
+
+
+face=pygame.Rect(100,200,200,200)
+
+eye1=pygame.Rect(120,240,60,30)
+eye2=pygame.Rect(220,240,60,30)
+mouth=pygame.Rect(170,340,60,30)
+
+
+while True:    
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+         
+    pygame.draw.rect(screen,(120,120,120),face)  
+    pygame.draw.rect(screen,(225,225,0),eye1) 
+    pygame.draw.rect(screen,(225,225,0),eye2)
+    pygame.draw.rect(screen,(0,225,0),mouth) 
+    pygame.display.update()
+    clock.tick(30)
+
+
+
